70e72fe922edbbd3a84f7771821849bc4bd9c6e4
tomoaki.yokobori <heychomoranma@yahoo.co.jp>
2020-03-23 13:54:21 +0000

    login

--- database/migrations/2020_03_23_220342_add_user_id_to_tasks.php
+++ database/migrations/2020_03_23_220342_add_user_id_to_tasks.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStatusToTasksTable extends Migration
+class AddUserIdToTasks extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,11 @@ class AddStatusToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status', 10);
+            $table->integer('user_id')->unsigned()->index();
+            
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
+            
         });
     }
 
@@ -26,7 +30,7 @@ class AddStatusToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumn('status');
+             $table->dropColumn('user_id');
         });
     }
 }
